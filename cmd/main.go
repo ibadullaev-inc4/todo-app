68b122fec739a68c9aa8f94f4d772623497a8ee5
Nariman Ibadullayev <n.ibadullaev@inc4.net>
2022-10-30 12:58:06 +0400

    add config and Makefile

--- cmd/main.go
+++ cmd/main.go
@@ -5,18 +5,26 @@ import (
 	"github.com/ibadullaev-inc4/todo-app/pkg/handler"
 	"github.com/ibadullaev-inc4/todo-app/pkg/repository"
 	"github.com/ibadullaev-inc4/todo-app/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
-	//handlers := new(handler.Handler)
-
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initialazing config: %s", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error ocured while running http server: %s\n", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("config")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
